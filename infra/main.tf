59f2055f4d03c44ede068add2d4070891d4d690b
AnnHelenGranholt <ann_helen@hotmail.no>
2023-11-16 19:53:23 +0100

    Utvidet container_ecr.yml til å kjøre terraform koden etter docker image er bygget , lagt til terraform provider og backend-konfigurasjon

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,10 @@
 resource "aws_apprunner_service" "service" {
-  service_name = "kjell-is-king"
+  service_name = "${var.prefix}-service"
 
   instance_configuration {
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
+    cpu = 256
+    memory = 1024
   }
 
   source_configuration {
@@ -11,9 +13,9 @@ resource "aws_apprunner_service" "service" {
     }
     image_repository {
       image_configuration {
-        port = "8080"
+        port = var.port
       }
-      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/kjell:latest"
+      image_identifier      = var.image_identifier
       image_repository_type = "ECR"
     }
     auto_deployments_enabled = true
@@ -21,7 +23,7 @@ resource "aws_apprunner_service" "service" {
 }
 
 resource "aws_iam_role" "role_for_apprunner_service" {
-  name               = "kjell-role-thingy"
+  name               = "${var.prefix}-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -60,8 +62,8 @@ data "aws_iam_policy_document" "policy" {
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "kjell-apr-policy-thingy"
-  description = "Policy for apprunner instance I think"
+  name        = "${var.prefix}-policy"
+  description = "Policy for apprunner instance"
   policy      = data.aws_iam_policy_document.policy.json
 }
 
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  type = string
+  default = "kandidatnr-2036"
+}
+variable "image_identifier" {
+  type = string
+}
+variable "port" {
+  type = string
+  default = "8080"
+}
